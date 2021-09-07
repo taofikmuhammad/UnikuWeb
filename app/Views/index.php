93f888c6003f56dec7aac79e6d9a5debb043abb0
asp05 <sugaraandi9@gmail.com>
2021-09-07 18:47:07 +0700

    cek

---
+++ app/Views/index.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?= base_url('assets/css/bootstrap.min.css'); ?>">
+    <title>Home</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-danger">
+        <a class="navbar-brand" href="#">Uniku</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Warga</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Provinsi</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Pengaturan</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <script src="<?= base_url('assets/js/bootstrap.min.js'); ?>"></script>
+</body>
+
+</html>
